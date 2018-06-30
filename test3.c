95d18f028c9e6a844a4c12ad4ecae9af795ca3b5
user-19 <38189524+user-19@users.noreply.github.com>
2018-06-30 16:52:04 +0530

    Test Case for Compiler Code

---
+++ test3.c
@@ -0,0 +1,26 @@
+switch(a)
+{
+	case 1:for(i=0;i<p;i+=1)
+		   {
+			for(h=0;h=o;h+=1)
+			{
+				a=a+b*s/d;
+			}
+			a=a+b;
+		   }
+		   break; 
+	case 2:while(k=c/s)
+		   {
+				for(i=0;i>p;i-=1)
+				{	
+					b=n*o+(d/r);					
+				}
+			k=1;
+		   }
+		   break; 
+	case 3:if(i+1) 
+		   then s=1; 
+		   else s=2;
+		   break; 
+	default:c=c+d;
+}
