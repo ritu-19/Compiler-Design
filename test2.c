5b01de36784ec8679494ab2c43e52ce85aeb3302
user-19 <38189524+user-19@users.noreply.github.com>
2018-06-30 16:51:51 +0530

    Test Case for Compiler Code

---
+++ test2.c
@@ -0,0 +1,26 @@
+switch(a)
+{
+	case 1:for(i=0;i<p;i+=1)
+		   {
+			a=a+b;
+		   }
+		   break; 
+	case 2:while(k=c/s)
+		   {
+				for(r=2;r>6;r-=1)
+				{
+				w=w*e*f;
+				}
+				k=1;
+		   }
+		   break; 
+	case 3:if(i+1) 
+		   then s=1; 
+		   else s=2;
+		   break; 
+	default:c=c+d;
+}
+for(j=1;j=r;j+=1)
+{
+	b=c*d/e;
+}
